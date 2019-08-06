8219a498f1dcecab5a27f61a86d16a7b905e8756
Artin <Artin@DESKTOP-L54PHGT.attlocal.net>
2019-08-05 22:07:01 -0700

    Working on diffuse still

--- src/Lighting/Diffuse.java
+++ src/Lighting/Diffuse.java
@@ -1,12 +1,19 @@
 package Lighting;
 
+import math.Vector3;
+
 public class Diffuse { 
-	calculateTriangleNormal(Vector3 v1,Vector3 v2,Vector3 v3) {
-		Vector U = v2-v1;
-		Vector V = v3-v1;
+	Vector3 Normal;
 	
-		Set Normal.x to (multiply U.y by V.z) minus (multiply U.z by V.y)
-		Set Normal.y to (multiply U.z by V.x) minus (multiply U.x by V.z)
-		Set Normal.z to (multiply U.x by V.y) minus (multiply U.y by V.x)
+	public static Vector3 calculateTriangleNormal(Vector3 v1,Vector3 v2,Vector3 v3) {
+		float normalx;
+		float normaly;
+		float normalz;
+		Vector3 U = Vector3.sub(v2,v1);
+		Vector3 V = Vector3.sub(v3,v1);
+		normalx = (U.y*V.z)-(U.z*V.y);
+		normaly = (U.z*V.x)-(U.x*V.z);
+		normalz = (U.x*V.y)-(U.y*V.x);
+		return new Vector3(normalx,normaly,normalz);
 	}
 }
--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -2,6 +2,7 @@ package runner;
 
 import java.util.ArrayList;
 
+import Lighting.Diffuse;
 import math.Matrix4;
 import math.Vector2;
 import math.Vector3;
@@ -16,16 +17,14 @@ public class Runner {
 	public Runner() {
 		display = new Display(720, 720);
 		display.clearBackBuffer(0, 0, 0, 1);
-				
-		ArrayList<Vector3> verts = new ObjReader().getVerticesFromObjFile("/Users/league/git/Tako_Engine_Software_Renderer/src/utilities/monkey.obj");
-		for(int i = 0;i<verts.size();i++) {
-			System.out.print(verts.get(i).x);
-			System.out.print(verts.get(i).y);
-			System.out.println();
-		}
+		Diffuse diffuse = new Diffuse();	
+	
+		Vector3 vec = Diffuse.calculateTriangleNormal(new Vector3(0.0f,1.0f,0.0f),new Vector3(1.0f,0.0f,0.0f),new Vector3(-1.0f,0.0f,0.0f));
+		System.out.println(vec.x);
+		System.out.println(vec.y);
+		System.out.println(vec.z);
 		display.renderTriangle(new Vector2(1.0f,0.0f),new Vector2(0.0f,1.0f),new Vector2(-1.0f,0.0f),new Vector4(255f, 0, 0, 255f));
-		display.renderTriangle(new Vector2(-1.0f, 0f),new Vector2(-0.75f,-0.75f),new Vector2(0.0f,0.0f),new Vector4(255f, 0, 0, 255f));
-		display.renderVertecies(verts);
+		
 		display.swapBuffers();
 	}
 	
